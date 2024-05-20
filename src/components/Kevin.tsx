bbf5c0a0cf14dafa21d5d95ebd07929156f652b7
KevinJara123 <kevinemanueljara796@gmail.com>
2024-05-20 12:23:40 -0300

    feature añadiendo componente kevin

---
+++ src/components/Kevin.tsx
@@ -0,0 +1,9 @@
+export function Kevin (){
+    return(
+        <>
+        <h1>Kevin Jara</h1>
+        <p>Estudiante de Programacion en la Universidad Tecnologica de Cordoba.</p>
+        </>
+    )
+    
+}
