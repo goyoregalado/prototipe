33f78e09c3315199b3c5b043117a0956b7c62c32
Juan Gregorio Regalado Pacheco <goyo.regalado@appfire.com>
2024-05-20 16:51:32 +0100

    refactor: sustituye los componentes por PersonalCard

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,54 @@ import { Kevin } from './components/Kevin'
 import { PersonalCard } from './components/PersonalCard'
 
 function App() {
+  const personList = [
+    {
+      fullName: 'Juan Gregorio Regalado Pacheco',
+      jobPosition: 'Senior Software Developer',
+      company: 'Appfire',
+      summary:
+        'Trabajo como desarrollador desde 2000, no había terminado la carrera y ya me tenían por ahí intentando desarrollar la siguiente gran app.',
+    },
+    {
+      fullName: 'Kevin Jara',
+      jobPosition: 'Estudiante de Programacion',
+      summary: 'Estudiante de Programacion en la Universidad Tecnologica de Cordoba.',
+    },
+    {
+      fullName: 'Joaquin',
+      jobPosition: 'Estudiante de Programacion',
+      summary: 'Estudiante de Programacion en la Universidad Tecnologica de Cordoba.',
+    }
+  ]
 
   return (
     <>
       <h1>This is the very beginning of SuperApps</h1>
       <p>Vamos a comenzar el plan de dominación mundial</p>
-      <Joaquin />
-      <Goyo />
-      <Kevin/>
+
       <PersonalCard
-        fullName="Juan Gregorio Regalado Pacheco"
-        jobPosition="Senior Software Developer"
-        company="Appfire"
-        summary="Trabajo como desarrollador desde 2000, no había terminado la carrera y ya me tenían por ahí intentando desarrollar la siguiente gran app."
+        fullName={personList[0].fullName}
+        jobPosition={personList[0].jobPosition}
+        company={personList[0].company}
+        summary={personList[0].summary}
       />
-      <PersonalCard
-        fullName="Kevin Jara"
-        jobPosition="Estudiante de Programacion"
-        summary="Estudiante de Programacion en la Universidad Tecnologica de Cordoba."
+
+
+    <PersonalCard
+        fullName={personList[1].fullName}
+        jobPosition={personList[1].jobPosition}
+        company={personList[1].company}
+        summary={personList[1].summary}
       />
+
+
+    <PersonalCard
+        fullName={personList[2].fullName}
+        jobPosition={personList[2].jobPosition}
+        company={personList[2].company}
+        summary={personList[2].summary}
+      />
+
     </>
   )
 }
