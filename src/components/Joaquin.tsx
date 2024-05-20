ae7d868ecd3836f2dd5da09dd20a4a8117304001
Galartza <jotaquin99g@gmail.com>
2024-05-20 12:19:03 -0300

    feature: agregando componente Joaquin

---
+++ src/components/Joaquin.tsx
@@ -0,0 +1,8 @@
+export function Joaquin () {
+    return (
+        <>
+            <h1>Joaquin Galarza</h1>
+            <p>Programador y estudiante de "Tecnico Superior en Desarrollo de Software."</p>
+        </>
+    )
+}
